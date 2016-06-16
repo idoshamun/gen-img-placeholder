1e130fc7fa2f3a8971d96e02d01852c31a9ca1f0
Ido Shamun <idoesh1@gmail.com>
2016-06-16 10:58:50 +0300

    add output placeholders to file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const program = require('commander');
+const fs = require('fs');
 const pkg = require('./package.json');
 const glob = require('glob');
 const lib = require('./lib');
@@ -13,7 +14,7 @@ program
     .description(pkg.description)
     .arguments('<pattern>')
     .option('-s, --size <percentage>', 'New size for the placeholder, in percentage', 1, parseInt)
-    .option('-o, --output <path>', 'Path for the output json', './placeholders.json')
+    .option('-o, --output <path>', 'Path for the output json')
     .parse(process.argv);
 
 if (!program.args.length) {
@@ -33,7 +34,12 @@ glob(program.args[0], (err, files) => {
 
     lib.generatePlaceholders(files, program.size)
         .then(placeholders => {
-            console.log(placeholders);
+            if (!program.output) {
+                console.log(placeholders);   
+            }
+            else {
+                fs.writeFileSync(program.output, JSON.stringify(placeholders));
+            }
             process.exit();
         })
         .catch(throwError);
